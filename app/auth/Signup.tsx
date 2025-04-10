42d017d7e3bd0a949fdb9995c4d2dfa3fb5aae7c
lioba00700 <lioba00700@naver.com>
2025-04-10 14:46:28 +0900

    feat: 회원가입 페이지 구현
    - 회원가입 페이지 퍼블리싱 완료

--- app/auth/Signup.tsx
+++ app/auth/Signup.tsx
@@ -1,6 +1,6 @@
 import ButtonCustom from "@/components/ui/ButtonCustom";
 import HeaderCustom from "@/components/ui/HeaderCustom";
-import { View, Text, StyleSheet } from "react-native"
+import { View, Text, Pressable, StyleSheet } from "react-native"
 
 import { Colors } from "@/constants/Colors";
 import InputCustom from "@/components/ui/InputCustom";
@@ -12,30 +12,36 @@ const Signup = () => {
             <Text style={[styles.text, {marginTop:100, marginLeft:48}]}>
                 회원가입
             </Text>
-            <InputCustom 
-                label="이메일"
-                placeholder="이메일을 입력하세요."
-                type="email"
-                isError={true}
-            />
-            <InputCustom 
-                label="인증번호"
-                placeholder="C2J3D2"
-                type="authNum"
-            />
-            <InputCustom 
-                label="비밀번호"
-                placeholder="*********"
-                type="password"
-            />
-            <InputCustom 
-                label="비밀번호 확인"
-                placeholder="*********"
-                type="password"
-            />
+            <View style={styles.inputContainer}>
+                <InputCustom 
+                    label="이메일"
+                    placeholder="이메일을 입력하세요."
+                    type="email"
+                    isError={true}
+                />
+                <InputCustom 
+                    label="인증번호"
+                    placeholder="C2J3D2"
+                    type="authNum"
+                />
+                <InputCustom 
+                    label="비밀번호"
+                    placeholder="*********"
+                    type="password"
+                />
+                <InputCustom 
+                    label="비밀번호 확인"
+                    placeholder="*********"
+                    type="password"
+                />
+            </View>
+            
             <ButtonCustom
                 text="회원가입"            
             />
+            <Pressable>
+                <Text style={styles.checkUser}>이미 회원이신가요</Text>
+            </Pressable>
         </View>
     )
 }
@@ -49,6 +55,18 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         fontFamily: 'Paperlogy',
         fontSize: 25,
+    },
+    inputContainer:{
+        marginVertical: 25
+    },
+    checkUser: {
+        color: '#25CCA0',
+        fontWeight: '500',
+        fontFamily: 'Paperlogy',
+        fontSize: 16,
+        justifyContent: 'center',
+        marginHorizontal: 'auto',
+        marginTop: 70,
     }
 })
 
