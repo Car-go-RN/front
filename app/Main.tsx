2866b7065122f1356b6755222b4a255b822516f1
lioba00700 <lioba00700@naver.com>
2025-04-13 00:29:55 +0900

    feat: 메인 화면 구현
    - buttonCustom outline 스타일 추가
    - 메인 화면 - 로그인 했을 때 구현
    - 메인 화면 - 로그인 안 했을 때 구현
    - 추천 휴게소 컴포넌트 구현

--- app/Main.tsx
+++ app/Main.tsx
@@ -1,13 +1,56 @@
-import { View } from "react-native";
+import { View, StyleSheet, Text, Image } from "react-native";
 import MainHeader from "@/components/ui/MainHeader";
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+import { Colors } from "@/constants/Colors";
+import MainContents from "@/components/ui/MainContents";
 
 const Main = () => {
     return(
         <View>
             <MainHeader />
+            <View style={styles.goRoutePage}>
+                <Image source={require('@/assets/images/rest-area.png')} style={styles.backgroundImage}/>
+                <View style={styles.goRouteDetail}>
+                    <FontAwesome5 name="pen" size={27} color={Colors.tint} />
+                    <View>
+                        <Text style={[styles.text,{fontSize: 12, fontWeight: 400, color:'#767676'}]}>지금 바로 휴게소를 알고 싶다면?</Text>
+                        <Text style={[styles.text,{fontSize: 16, fontWeight:600, marginVertical: 5}]}>내 경로에 있는 휴게소 보기</Text>
+                    </View>
+                </View>
+            </View>
+            <MainContents isLogin={false}/>
         </View>
     )
 
 }
+
+const styles = StyleSheet.create({
+    goRoutePage: {
+        marginTop: 30,
+        position:'relative'
+    },
+    backgroundImage: {
+        width:'85%',
+        height: 100,
+        margin:'auto',
+        borderRadius: 5,
+    },
+    goRouteDetail: {
+        display: 'flex',
+        flexDirection:'row',
+        alignItems:'center',
+        width: '85%',
+        height: 100,
+        position:'absolute',
+        backgroundColor: 'white',
+        opacity: 0.7,
+        alignSelf:'center',
+        justifyContent:'center'
+    },
+    text:{
+        fontFamily:'Paperlogy',
+        marginLeft: 15
+    }
+});
 
 export default Main;
---
+++ components/ui/MainRestItem.tsx
@@ -0,0 +1,82 @@
+import { Colors } from "@/constants/Colors"
+import { Image, StyleSheet, Text, View } from "react-native"
+import AntDesign from '@expo/vector-icons/AntDesign';
+import Entypo from '@expo/vector-icons/Entypo';
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+type Brand = {
+    name: string,
+    logo: string
+}
+
+type Amenities = {
+    name: string,
+    icon: string
+}
+
+type restItemProps = {
+    img: string,
+    name: string,
+    review: number,
+    via: number,
+    gas: number,
+    brands: Brand[],
+    amenities: Amenities[]
+}
+
+const MainRestItem = () => {
+    return(
+        <View style={styles.restItem}>
+            <Image style={styles.restImg} source={require('@/assets/images/test-rest-area.png')}/>
+            <View style={styles.restDetail}>
+                <Text style={[styles.text,{fontSize:15}]}>동명휴게소(춘천방향)</Text>
+                <Text style={[styles.text,{fontSize:13, color: Colors.yellow}]}>★★★★☆</Text>
+                <Text style={[styles.text,{marginVertical:3}]}>경유 1,234  휘발유 1,352</Text>
+                <Text style={[styles.text,{marginVertical:3}]}>브랜드</Text>
+                <Text style={[styles.text,{marginVertical:3}]}>편의시설</Text>
+            </View>
+            <View style={styles.reaction}>
+                <AntDesign name="heart" size={15} color={Colors.lightGrey} style={styles.icon} /><Text style={styles.reactState}>12</Text>
+                <Entypo name="message" size={15} color={Colors.lightGrey} style={styles.icon} /><Text style={styles.reactState}>12</Text>
+                <Ionicons name="bookmark" size={15} color={Colors.lightGrey} style={styles.icon} /><Text style={styles.reactState}>12</Text>
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    restItem: {
+        width: 180,
+        height: 240,
+        backgroundColor: 'white',
+        borderRadius: 5,
+        overflow: 'hidden',
+        marginRight: 15
+    },
+    restImg: {
+        width: '100%',
+        height: 100
+    },
+    restDetail: {
+        padding: 11,
+    },
+    text: {
+        fontFamily: 'Paperlogy',
+        fontWeight: 500,
+        fontSize: 10,
+    },
+    reaction: {
+        flexDirection: 'row',
+        marginLeft:10
+    },
+    icon: {
+        marginRight: 3
+    },
+    reactState: {
+        color: Colors.lightGrey,
+        width: 25,
+        fontSize: 12
+    }
+});
+
+export default MainRestItem;
