e548d0b704b3a10355dd0ab2aa270004001c0778
신수지 <155224169+3suji3@users.noreply.github.com>
2025-04-15 09:03:07 +0900

    Revert "내 경로에 있는 휴게소 보기 "

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,3 +1,4 @@
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
@@ -5,17 +6,19 @@ import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
 
+import { useColorScheme } from '@/hooks/useColorScheme';
+
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-    Paperlogy: require('../assets/fonts/Paperlogy-4Regular.ttf'),
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
 
   useEffect(() => {
     if (loaded) {
-
       SplashScreen.hideAsync();
     }
   }, [loaded]);
@@ -25,19 +28,12 @@ export default function RootLayout() {
   }
 
   return (
-    <>
-      <Stack
-        initialRouteName="Splash"
-        screenOptions={{ headerShown: false }}
-      >
-        <Stack.Screen name="Splash" />
-        <Stack.Screen name="auth/Login" />
-        <Stack.Screen name="auth/Signup" />
-        <Stack.Screen name="Main" />
-        <Stack.Screen name="search/RouteQuest" />
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
       <StatusBar style="auto" />
-    </>
+    </ThemeProvider>
   );
 }
