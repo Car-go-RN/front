710ffdb8fccf2e62e5e4264dfa09ef32d973bb94
lioba00700 <lioba00700@naver.com>
2025-05-26 13:46:20 +0900

    chore: 태그 아이콘 수정
    - 태그에 맞는 아이콘 표시 (편의시설 태그일 때)
    - 태그에 맞는 브랜드 이미지 표시 (브랜드 태그일 때)

---
+++ constants/TagMock.ts
@@ -0,0 +1,40 @@
+
+export const gas = [
+    {name: '전기 충전소', icon:'car-electric'},
+    {name: '수소 충전소', icon:'hydrogen-station' },
+    {name: '주유소', icon: 'gas-station'}
+]
+
+export const brands = [
+    {name: 'CU', icon: 'CU', img: require('@/assets/brands/CU.png')},
+    {name: 'GS25', icon: 'GS25',img: require('@/assets/brands/GS25.png')},
+    {name: '세븐일레븐', icon:'세븐일레븐',img: require('@/assets/brands/세븐일레븐.png')},
+    {name: '던킨도너츠', icon: '던킨도너츠',img: require('@/assets/brands/던킨도너츠.png')},
+    {name: 'KFC', icon: 'KFC',img: require('@/assets/brands/KFC.png')},
+    {name: '공차', icon: '공차', img: require('@/assets/brands/공차.png')},
+    {name: '투썸', icon: '투썸플레이스', img: require('@/assets/brands/투썸플레이스.png')},
+    {name: '할리스', icon: '할리스커피', img: require('@/assets/brands/할리스커피.png')},
+    {name: '엔제리너스', icon:'엔제리너스', img: require('@/assets/brands/엔제리너스.png')},
+    {name: '신전떡볶이', icon:'신전떡볶이', img: require('@/assets/brands/신전떡볶이.png')},
+    {name: '33떡볶이', icon: '33떡볶이', img: require('@/assets/brands/33떡볶이.png')},
+    {name: '베스킨라빈스', icon: '베스킨라빈스', img: require('@/assets/brands/베스킨라빈스.png')},
+    {name: '스타벅스', icon: '스타벅스', img: require('@/assets/brands/스타벅스.png')},
+    {name: '롯데리아', icon: '롯데리아', img: require('@/assets/brands/롯데리아.png')},
+    {name: '도미노피자', icon: '도미노피자', img: require('@/assets/brands/도미노피자.png')},
+    {name: 'BBQ 치킨', icon:'BBQ', img: require('@/assets/brands/BBQ.png')},
+    {name: '파리바게트', icon:'파리바게트', img: require('@/assets/brands/파리바게트.png')},
+]
+
+export const amenities = [
+    {name: '샤워실',  icon:'shower'},
+    {name: '쉼터', icon:'pine-tree'},
+    {name: '약국', icon:'pill'},
+    {name: '세차장', icon:'car-wash'},
+    {name: '수유실', icon:'baby-bottle-outline'},
+    {name: '수면실', icon:'sleep'},
+    {name: '병원', icon:'hospital-box-outline'},
+    {name: '세탁실', icon:'washing-machine'},
+    {name: '화물차 라운지', icon:'truck'},
+    {name: '경정비소', icon:'car-cog'},
+    {name: '농산물 판매장', icon:'sprout'},
+]
