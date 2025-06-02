6aa471159ef4cd91c40850d675f9422c8b8e491f
3suji3 <tnwl2799@gmail.com>
2025-06-02 10:14:41 +0900

    feat: redux toolkit
    변경 사항
    - user 관련 redux 작성

---
+++ app/store/slices/userSlices.ts
@@ -0,0 +1,37 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface User {
+  email: string
+}
+
+interface AuthState {
+  token: string | null
+  user: User | null
+  isLoggedIn: boolean
+}
+
+const initialState: AuthState = {
+  token: null,
+  user: null,
+  isLoggedIn: false,
+}
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    loginSuccess(state, action: PayloadAction<{token: string; user: User}>) {
+      state.token = action.payload.token
+      state.user = action.payload.user
+      state.isLoggedIn = true
+    },
+    logout(state) {
+      state.token = null
+      state.user = null
+      state.isLoggedIn = false
+    },
+  },
+})
+
+export const { loginSuccess, logout } = authSlice.actions
+export default authSlice.reducer
